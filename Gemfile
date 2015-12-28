2fc004c4c06284f371f595c1bd45b27fe5eb9f47
Ivan Malykh <ivan@lesslines.com>
2015-12-28 20:58:54 +0100

    add codeclimate coverage analyzer

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in nbb.gemspec
 gemspec
+
+group :test do
+  gem "codeclimate-test-reporter", '~> 0.4'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'nbb'
 require 'webmock/rspec'
