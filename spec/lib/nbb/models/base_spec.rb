985f9ac6f99488e2c6ec178fd3be071c96397888
Ivan Malykh <ivan@lesslines.com>
2016-01-24 14:05:17 +0100

    extract initialization to base class

---
+++ spec/lib/nbb/models/base_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+module Nbb
+  module Models
+    class TestEntity < Nbb::Models::Base
+      ATTRIBUTES = [:foo]
+      attr_accessor(*ATTRIBUTES)
+    end
+  end
+end
+
+describe Nbb::Models::Base do
+  it 'infers classname of inherited class' do
+    test_class = Nbb::Models::TestEntity.new(foo: :bar)
+    expect(test_class.foo).to eq :bar
+  end
+end
